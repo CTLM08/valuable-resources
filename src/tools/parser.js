7f413e3d5161c66dd138393b2a4ed95b1a99e3ee
melvinchia3636 <melvinchia623600@gmail.com>
2023-02-26 12:02:33 +0800

    code refactor

--- src/tools/parser.js
+++ src/tools/parser.js
@@ -1,11 +1,25 @@
 const parser = new DOMParser();
-export const getOgImage =async (url)=>{
-    return await fetch(`https://cors-anywhere.thecodeblog.net/${url}`).then(e => e.text()).then((html) =>{
-        const doc = parser.parseFromString(html, "text/html");
-        const ogImage = doc.querySelector("meta[property='og:image']");
-        if(ogImage){
-            return ogImage.content;
-        }
-        return null;
-    })
-}
+
+const parseHTML = (html) => {
+  const doc = parser.parseFromString(html, 'text/html');
+  const ogImage = doc.querySelector("meta[property='og:image'],meta[name='og:image']");
+  if (ogImage) {
+    return ogImage.content;
+  }
+  return null;
+};
+
+export default async (url) => {
+  try {
+    const img = await fetch(`https://${url.split('://')[1] ?? url}`)
+      .then((e) => e.text())
+      .then(parseHTML);
+    return img;
+  } catch {
+    console.log('hmm');
+    const img = await fetch(`https://cors-anywhere.thecodeblog.net/${url.split('://')[1] ?? url}`)
+      .then((e) => e.text())
+      .then(parseHTML);
+    return img;
+  }
+};
