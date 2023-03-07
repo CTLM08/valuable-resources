0d04d57a82da368eaa74d54fcdf095fb45b168cc
melvinchia3636 <melvinchia623600@gmail.com>
2023-03-07 11:18:47 +0800

    hmm

--- src/tools/parser.js
+++ src/tools/parser.js
@@ -3,7 +3,6 @@ const parser = new DOMParser();
 const parseHTML = (html) => {
   const doc = parser.parseFromString(html, 'text/html');
   const ogImage = doc.querySelector("meta[property='og:image'],meta[name='og:image']");
-  console.log(ogImage);
   if (ogImage) {
     return ogImage.content;
   }
@@ -13,11 +12,15 @@ const parseHTML = (html) => {
 export default async (url) => {
   try {
     const img = await fetch(`https://${url.split('://')[1] ?? url}`)
-      .then((e) => e.text())
+      .then((e) => {
+        if (e.status === 400) {
+          throw new Error('hmm');
+        }
+        return e.text();
+      })
       .then(parseHTML);
     return img;
   } catch {
-    console.log('hmm');
     const img = await fetch(`https://cors-anywhere.thecodeblog.net/${url.split('://')[1] ?? url}`)
       .then((e) => e.text())
       .then(parseHTML);
